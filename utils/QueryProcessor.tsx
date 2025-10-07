5fa6cf09c07aebf46f401c172186c9d2a9fca795
Evelyn Lui <evelynlu@andrew.cmu.edu>
2025-10-07 10:18:29 -0400

    x + y

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -14,6 +14,13 @@ export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("andrew id")) {
     return "evelynlu";
   }
+  
+  const match = query.match(/what\s+is\s+(-?\d+)\s*\+\s*(-?\d+)\??/i);
+  if (match) {
+    const x = parseFloat(match[1]);
+    const y = parseFloat(match[2]);
+    return (x + y).toString();
+  }
 
   return "";
 }
